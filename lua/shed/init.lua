6cbe8ea44a98ec2cda0baff1b4da6d84b686df25
Ujan RoyBandyopadhyay <116058173+ujan-r@users.noreply.github.com>
2025-12-07 12:03:38 -0600

    Start client from plugin

--- lua/shed/init.lua
+++ lua/shed/init.lua
@@ -3,6 +3,7 @@ local M = {}
 local api = vim.api
 
 function M.attach(bufnr)
+	local stdin = vim.system({"shed-client"}, { stdin = true, stdout = function() end })
 	api.nvim_buf_attach(bufnr, false, {
 		on_lines = function ()
 		end

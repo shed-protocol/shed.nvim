84777fdaaca505ed5b30ca3390a3b8d91783aa5b
Ujan RoyBandyopadhyay <116058173+ujan-r@users.noreply.github.com>
2025-12-09 03:31:36 -0600

    Send local changes to client (#13)

--- lua/shed/init.lua
+++ lua/shed/init.lua
@@ -7,24 +7,36 @@ local api = vim.api
 local json = vim.json
 
 function M.attach(bufnr)
+	local skip = false
+
 	local buf = ""
-	local client = vim.system({"shed-client", "localhost:9042"}, {
+	local client = vim.system({ "shed-client", "localhost:9042" }, {
 		stdin = true,
 		stdout = function(_, data)
+			if not data then return end
 			buf = buf .. data
 			local msg
 			msg, buf = transport.read(buf)
 			if msg then
 				local op = json.decode(msg).body.op
-				vim.schedule(function () operation.apply(op) end)
+				vim.schedule(function()
+					skip = true
+					operation.apply(op)
+					skip = false
+				end)
 			end
 		end
 	})
 
 	api.nvim_buf_attach(bufnr, false, {
-		on_lines = function ()
-			local json = '{"kind":1,"body":{"op":{"type":"deletion","pos":20,"len":0}}}'
-			transport.write(json, client)
+		on_lines = function(_, _, _, firstline, _, new_lastline, old_byte_size)
+			-- TODO: Fix race condition (skip only edits equal to last remote change?)
+			if skip then return end
+			local ops = operation.from(firstline, new_lastline, old_byte_size)
+			for _, op in ipairs(ops) do
+				local content = json.encode({ kind = 1, body = { op = op } })
+				transport.write(content, client)
+			end
 		end
 	})
 end

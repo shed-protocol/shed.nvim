e33ec0e4b0711e9571e56c04cc198049a76cc020
Ujan RoyBandyopadhyay <116058173+ujan-r@users.noreply.github.com>
2025-12-09 01:43:34 -0600

    Don't relay remote updates as local changes

--- lua/shed/init.lua
+++ lua/shed/init.lua
@@ -7,8 +7,10 @@ local api = vim.api
 local json = vim.json
 
 function M.attach(bufnr)
+	local skip = false
+
 	local buf = ""
-	local client = vim.system({"shed-client", "localhost:9042"}, {
+	local client = vim.system({ "shed-client", "localhost:9042" }, {
 		stdin = true,
 		stdout = function(_, data)
 			buf = buf .. data
@@ -16,15 +18,20 @@ function M.attach(bufnr)
 			msg, buf = transport.read(buf)
 			if msg then
 				local op = json.decode(msg).body.op
-				vim.schedule(function () operation.apply(op) end)
+				vim.schedule(function()
+					skip = true
+					operation.apply(op)
+					skip = false
+				end)
 			end
 		end
 	})
 
 	api.nvim_buf_attach(bufnr, false, {
-		on_lines = function ()
+		on_lines = function()
 			local json = '{"kind":1,"body":{"op":{"type":"deletion","pos":20,"len":0}}}'
-			transport.write(json, client)
+			-- TODO: Fix race condition (skip only edits equal to last remote change?)
+			if not skip then transport.write(json, client) end
 		end
 	})
 end
